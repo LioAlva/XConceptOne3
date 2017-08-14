28209bc200504ea6aae3a302e2a6a8dc0b7cc915
Luis Alvarez Caballero <luisperso_015@hotmail.com>
2017-08-14 13:56:59 -0500

    video 51 solo muestra como  funciona en el mainPage

---
+++ XConceptOne3/XConceptOne3/XConceptOne3/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XConceptOne3
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            Padding = Device.OnPlatform(
+                new Thickness(10, 20, 10, 10),
+                new Thickness(10),
+                new Thickness(10));
+
+        }
+	}
+}
